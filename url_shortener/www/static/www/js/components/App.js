bde82a0c6946d7e7361a10be1c894c57b252f325
Jonathan Trope <jonathan@jonathantrope.com>
2018-11-28 22:16:07 -0500

    Moar FE

--- url_shortener/www/static/www/js/components/App.js
+++ url_shortener/www/static/www/js/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import request from 'superagent';
-import {getCookie} from "../utils";
+import request from "superagent";
+import { getCookie } from "../utils";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             shortened: null,
-            original: '',
+            original: "",
             errMsg: null
         };
 
@@ -15,41 +15,45 @@ class App extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.createShortenedURL = this.createShortenedURL.bind(this);
+        this.renderError = this.renderError.bind(this);
+        this.renderShortenedURL = this.renderShortenedURL.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div>
+                {this.renderError()}
+                <form onSubmit={this.handleSubmit}>
+                        <input className="url-input" type="text" value={this.state.original}
+                               onChange={this.handleChange}/>
+                    <input className="submit-btn" type="submit" value="Shorten" />
+                </form>
+                {this.renderShortenedURL()}
+                </div>
+            </div>
+        );
     }
 
     createShortenedURL(url) {
         // TODO: Put base url in config
         const _this = this;
-        const csrfToken = getCookie('csrftoken');
+        const csrfToken = getCookie("csrftoken");
         request
             .post("http://127.0.0.1:3000/api/shortened_urls/")
             .send({url: url})
-            .set('X-CSRFToken', csrfToken)
+            .set("X-CSRFToken", csrfToken)
             .end(function (err, res) {
                 const data = res.body;
                 if (err) {
                     _this.setState({errMsg: data.error})
                 } else {
                     _this.setState({
-                        shortened: data.shortened_url
+                        shortened: data.shortened_url,
+                        errMsg: null
                     })
                 }
             });
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="text" value={this.state.original}
-                               onChange={this.handleChange}/>
-                    </label>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </div>
-        );
     }
 
     handleChange(event) {
@@ -59,6 +63,29 @@ class App extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         this.createShortenedURL(this.state.original);
+    }
+
+    renderError() {
+        if (this.state.errMsg) {
+            return (
+                <p className="error-msg">Error: {this.state.errMsg}</p>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    renderShortenedURL() {
+        if (this.state.shortened) {
+            return (
+                <div>
+                    Here is your shortened url:&nbsp;
+                    <a href={this.state.shortened} target="_blank">{this.state.shortened}</a>
+                </div>
+            );
+        } else {
+            return null;
+        }
     }
 }
 
